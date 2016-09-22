5c6daeecb11a3b7723912695fabda44b9dd5e5fb
mennitise <mennitise@gmail.com>
2016-09-21 21:47:09 -0300

    Add abstract class SetOfValues.

---
+++ src/main/java/ar/fiuba/tdd/template/SetOfValues.java
@@ -0,0 +1,22 @@
+package ar.fiuba.tdd.template;
+
+import java.util.ArrayList;
+
+abstract class SetOfValues {
+
+    private ArrayList<Object> values;
+
+    SetOfValues() {
+        this.values = new ArrayList<>();
+    }
+
+    void insertValue(Object object) {
+        this.values.add(object);
+    }
+
+    void deleteValue(Object object) {
+        this.values.remove(object); // TODO: object.getValue()
+    }
+
+    abstract boolean canInsertValue(Object object);
+}
