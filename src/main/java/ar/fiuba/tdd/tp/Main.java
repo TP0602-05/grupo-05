8729e2bf746a89410d26add55080186231757381
Nico Moccagatta <nicomoccagatta@gmail.com>
2016-09-24 19:06:04 -0300

    Add json Parser

--- src/main/java/ar/fiuba/tdd/tp/Main.java
+++ src/main/java/ar/fiuba/tdd/tp/Main.java
@@ -1,13 +1,42 @@
 package ar.fiuba.tdd.tp;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import ar.fiuba.tdd.tp.model.Grid;
+import java.io.FileReader;
+import java.io.IOException;
 
-/**
- * Created by nicolas on 21/09/16.
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is just a tp project");
+
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            String path = System.getProperty("user.dir") + "/src/main/java/ar/fiuba/tdd/tp/model/" + "input" + ".json";
+            System.out.println(path);
+
+            Object obj = parser.parse(new FileReader(path));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            String name = (String) jsonObject.get("name");
+            System.out.println(name);
+
+            long age = (Long) jsonObject.get("age");
+            System.out.println(age);
+
+            // loop array
+            JSONArray msg = (JSONArray) jsonObject.get("messages");
+            for (Object aMsg : msg) {
+                System.out.println(aMsg);
+            }
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
