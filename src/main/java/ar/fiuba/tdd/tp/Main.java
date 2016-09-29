78987adcaf072c60b03c35cc190c76b8be483d3e
Samanta <samanta@darriens.com>
2016-09-29 14:09:07 -0300

    Load game by input string

--- src/main/java/ar/fiuba/tdd/tp/Main.java
+++ src/main/java/ar/fiuba/tdd/tp/Main.java
@@ -2,9 +2,23 @@ package ar.fiuba.tdd.tp;
 
 import ar.fiuba.tdd.tp.model.Game;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        Game.init("kakuro");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Welcome. Please enter a game name:");
+        String gameName = null;
+        try {
+            gameName = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("You will play : " + gameName);
+        Game.init(gameName);
 
     }
 }
