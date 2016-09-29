33424b8b85ee0e578c7a63cdfeb00841f36499ce
joaquin <segui.joaquin@gmail.com>
2016-09-29 18:15:50 -0300

    Fixed checkstyle errors - Closes #10

---
+++ src/main/java/ar/fiuba/tdd/tp/controller/MouseController.java
@@ -0,0 +1,54 @@
+package ar.fiuba.tdd.tp.controller;
+
+import ar.fiuba.tdd.tp.model.Game;
+import ar.fiuba.tdd.tp.model.cell.Value;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.*;
+
+public class MouseController extends MouseAdapter {
+    private int row;
+    private int col;
+
+    public MouseController(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    private JFrame setFrameInput() {
+        JFrame frameInput = new JFrame();
+        frameInput.setLayout(new BorderLayout());
+        frameInput.setBounds(100, 100, 100, 100);
+        return frameInput;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
+        if (!Game.getInstance().getCell(row, col).isBlocked()) {
+            if (event.getButton() == MouseEvent.BUTTON1) {
+                JFrame frameInput = setFrameInput();
+                JPanel jp = new JPanel();
+                JTextField textField = new JTextField();
+                textField.setColumns(5);
+                textField.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        frameInput.dispose();
+                        Value value = new Value(Integer.parseInt(textField.getText()));
+                        Game.getInstance().setValue(row, col, value);
+                    }
+                });
+                jp.add(textField);
+                frameInput.add(jp);
+                frameInput.setLocationRelativeTo(null);
+                frameInput.setVisible(true);
+            } else if (event.getButton() == MouseEvent.BUTTON3) {
+                Game.getInstance().deleteValue(row, col);
+            }
+        }
+    }
+}
--- src/main/java/ar/fiuba/tdd/tp/view/InputButton.java
+++ src/main/java/ar/fiuba/tdd/tp/view/InputButton.java
@@ -1,52 +1,17 @@
 package ar.fiuba.tdd.tp.view;
 
+import ar.fiuba.tdd.tp.controller.MouseController;
 import ar.fiuba.tdd.tp.model.Game;
 import ar.fiuba.tdd.tp.model.cell.Value;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import javax.swing.*;
 
 public class InputButton extends JButton {
 
     //TODO: Dividir esto, NCSS max es 20
     public InputButton(Value value, int row, int col) {
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent event) {
-                if (!Game.getInstance().getCell(row, col).isBlocked()) {
-                    if (event.getButton() == MouseEvent.BUTTON1) {
-                        JFrame frameInput = new JFrame();
-                        frameInput.setLayout(new BorderLayout());
-                        frameInput.setBounds(100, 100, 100, 100);
-
-                        JPanel jp = new JPanel();
-                        JTextField textField = new JTextField();
-                        textField.setColumns(5);
-                        JButton botonOK = new JButton("OK");
-                        botonOK.addActionListener(new ActionListener() {
-                            @Override
-                            public void actionPerformed(ActionEvent actionEvent) {
-                                frameInput.dispose();
-                                Value value = new Value(Integer.parseInt(textField.getText()));
-                                Game.getInstance().setValue(row, col, value);
-                            }
-                        });
-
-                        jp.add(textField);
-                        jp.add(botonOK);
-                        frameInput.add(jp);
-                        frameInput.setLocationRelativeTo(null);
-                        frameInput.setVisible(true);
-                    } else if (event.getButton() == MouseEvent.BUTTON3) {
-                        Game.getInstance().deleteValue(row, col);
-                    }
-                }
-            }
-        });
+        addMouseListener(new MouseController(row,col));
 
         Value valueAux = Game.getInstance().getValue(row,col);
         if (valueAux.getValue() != 0) {
