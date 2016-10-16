5849dd7e511f3c44e3070e4ea2589f40de5cfcbb
mennitise <mennitise@gmail.com>
2016-10-15 23:38:10 -0300

    Add dots to class Value

--- src/test/java/ar/fiuba/tdd/tp/model/cell/ValueTests.java
+++ src/test/java/ar/fiuba/tdd/tp/model/cell/ValueTests.java
@@ -2,27 +2,81 @@ package ar.fiuba.tdd.tp.model.cell;
 
 import org.junit.Test;
 
+import java.util.Vector;
+
 import static org.junit.Assert.*;
 
 public class ValueTests {
 
+    private void addNBooleansTo(Vector<Boolean> dots, Boolean bool, int n) {
+        for ( int i = 0; i<=n; i++ ) {
+            dots.add(bool);
+        }
+    }
+
     @Test
-    public void testCreateValue() {
+    public void testCreateValueInteger() {
         Value myValue = new Value(3);
         assertTrue( myValue.getValue() == 3 );
     }
 
     @Test
-    public void testCreateValuesWithTheSameContent() {
+    public void testCreateValuesIntegerWithTheSameContent() {
         Value myValue1 = new Value(7);
         Value myValue2 = new Value(7);
         assertTrue( myValue1.isEqualTo(myValue2) );
     }
 
     @Test
-    public void testCreateValuesWithDifferentContent() {
+    public void testCreateValuesIntegerWithDifferentContent() {
         Value myValue1 = new Value(7);
         Value myValue2 = new Value(3);
         assertFalse( myValue1.isEqualTo(myValue2) );
+    }
+
+    @Test
+    public void testCreateValueDotsWithSameContent() {
+        Vector<Boolean> dots1 = new Vector<>();
+        this.addNBooleansTo(dots1, false, 9);
+        Value myValue1 = new Value(dots1);
+        Vector<Boolean> dots2 = new Vector<>();
+        this.addNBooleansTo(dots2, false, 9);
+        Value myValue2 =new Value(dots2);
+        assertTrue(myValue1.isDotsEqualTo(myValue2));
+    }
+
+    @Test
+    public void testCreateValueDotsWithDifferentContent() {
+        Vector<Boolean> dots1 = new Vector<>();
+        this.addNBooleansTo(dots1, false, 9);
+        Value myValue1 = new Value(dots1);
+        Vector<Boolean> dots2 = new Vector<>();
+        this.addNBooleansTo(dots2, false, 5);
+        this.addNBooleansTo(dots2, true, 4);
+        Value myValue2 =new Value(dots2);
+        assertFalse(myValue1.isDotsEqualTo(myValue2));
+    }
+
+    @Test
+    public void testCombineValueDots() {
+        Vector<Boolean> dots1 = new Vector<>();
+        this.addNBooleansTo(dots1, true, 1);
+        this.addNBooleansTo(dots1, false, 8);
+        Value myValue1 = new Value(dots1);
+
+        Vector<Boolean> dots2 = new Vector<>();
+        this.addNBooleansTo(dots2, false, 8);
+        this.addNBooleansTo(dots2, true, 1);
+        Value myValue2 =new Value(dots2);
+
+        Vector<Boolean> dots3 = new Vector<>();
+        this.addNBooleansTo(dots3, true, 1);
+        this.addNBooleansTo(dots3, false, 7);
+        this.addNBooleansTo(dots3, true, 1);
+        Value myValue3 =new Value(dots3);
+
+        myValue1.combineDots(myValue2);
+
+        assertTrue(myValue1.isDotsEqualTo(myValue3));
     }
 }
