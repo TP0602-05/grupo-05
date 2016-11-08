8f1b6612cf0206cedaba54b251fec2431492aefb
Nico Moccagatta <nicomoccagatta@gmail.com>
2016-11-08 19:16:31 -0300

    CheckAdjacentRule finished, hitori finished!

--- src/main/java/ar/fiuba/tdd/tp/model/rule/CheckAdjacentRule.java
+++ src/main/java/ar/fiuba/tdd/tp/model/rule/CheckAdjacentRule.java
@@ -1,13 +1,15 @@
 package ar.fiuba.tdd.tp.model.rule;
 
+import ar.fiuba.tdd.tp.model.Game;
 import ar.fiuba.tdd.tp.model.cell.PositionValueDuo;
 import ar.fiuba.tdd.tp.model.cell.Value;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class CheckAdjacentRule implements Rule {
 
-    int apply;
+    private int apply;
 
     public CheckAdjacentRule(int value) {
         this.apply = value;
@@ -18,21 +20,66 @@ public class CheckAdjacentRule implements Rule {
         if (this.apply == 0) {
             return true;
         }
-        int size = values.size();
-        PositionValueDuo lastSet = values.get(size - 1);
-        lastSet.print();
+        boolean checkCondition = true;
+        if (isBlackCell(value.getValue())) {
+            checkCondition = checkRuleCondition(values,value);
+        }
+        return checkCondition;
+    }
 
+    private boolean checkRuleCondition(ArrayList<PositionValueDuo> values, PositionValueDuo value) {
+        for (PositionValueDuo otherValue : values) {
+            if (areAdjacent(otherValue,value)) {
+                if (isBlackCell(otherValue.getValue())) {
+                    return false;
+                } else {
+                    if (!hasWayOut(otherValue,values)) {
+                        return false;
+                    }
+                }
+            }
+        }
         return true;
+    }
+
+    private boolean hasWayOut(PositionValueDuo otherValue, ArrayList<PositionValueDuo> values) {
+        int posibleWaysOut = 0;
+        for (PositionValueDuo otherValue2 : values) {
+            if (areAdjacent(otherValue,otherValue2)) {
+                if (!isBlackCell(otherValue2.getValue())) {
+                    posibleWaysOut++;
+                }
+            }
+        }
+        return (posibleWaysOut >= 1);
+    }
+
+    private boolean areAdjacent(PositionValueDuo otherValue, PositionValueDuo value) {
+        int difCol = Math.abs(otherValue.getPos().getCol() - value.getPos().getCol());
+        int difRow = Math.abs(otherValue.getPos().getRow() - value.getPos().getRow());
+
+        // if they are adjacents
+        if (((difRow == 1) && (difCol == 0)) || ((difRow == 0) && (difCol == 1))) {
+            return true;
+        }
+        return false;
+    }
+
+    private Boolean isBlackCell(Value value) {
+        Vector<Boolean> dots = value.getDots();
+        Boolean line = dots.get(0) && dots.get(1) && dots.get(2);
+
+        return line && dots.get(3) && dots.get(4) && dots.get(5)
+                && dots.get(6) && dots.get(7) && dots.get(8);
     }
 
     @Override
     public boolean checkFinal(ArrayList<PositionValueDuo> values) {
-        return false;
+        return true;
     }
 
     @Override
     public void printRule() {
         System.out.println("CheckAdjacents rules");
-
     }
 }
